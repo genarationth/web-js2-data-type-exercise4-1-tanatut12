c29d5b2d8d01bbb8968df1e946bf6217b3e3c214
tanatut12 <tanatut12@gmail.com>
2024-05-13 14:14:22 +0700

    finish

---
+++ script.js
@@ -0,0 +1,27 @@
+const product = {
+    product_id: 1,
+    product_name: "ตราม้า",
+    quantity_in_stock: 1,
+    unit_price: 20,
+    product_description: {
+        product_brand: "ม้า",
+        size_or_quantity: 1,
+        taste_or_scent: null,
+        colour: "blue",
+        additional_description: null
+    }
+}
+
+console.log(product.product_id);
+console.log(product.product_name);
+console.log(product.quantity_in_stock);
+console.log(product.unit_price);
+console.log(product.product_description);
+
+
+
+
+
+
+
+
